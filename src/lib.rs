a61519afdf8875ea69a3169eaba8f3b38bebd45f
eigo-no-sensei <auladinglese@gmail.com>
2026-05-24 19:30:55 +0100

    Add ParakeetJA to public exports

--- src/lib.rs
+++ src/lib.rs
@@ -79,6 +79,7 @@ pub use error::{Error, Result};
 pub use execution::{ExecutionProvider, ModelConfig as ExecutionConfig};
 pub use parakeet::Parakeet;
 pub use parakeet_tdt::ParakeetTDT;
+pub use parakeet_ja::ParakeetJA;
 pub use timestamps::TimestampMode;
 pub use transcriber::*;
 
